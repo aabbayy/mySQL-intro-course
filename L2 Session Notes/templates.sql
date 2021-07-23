0aabbeb64b56a734e48efe52fe24b16518485ee0
abbydurbridge <abby.durbridge@gmail.com>
2021-07-23 15:48:21 +1000

    Update file structure, new JIRA templates

---
+++ L2 Session Notes/templates.sql
@@ -0,0 +1,43 @@
+/* Duplicate Custom Enrolment Form*/
+
+{
+    "service_id" : ,
+    "service_ids" []
+}
+
+/*Cloning Occurrence*/
+
+{
+  "enrolment_id" : " ",
+  "base_occurrence" : "1",
+  "reason" : "",
+  "pea_reason" : "",
+  "date" : {
+      "start" : "2021-07-01",
+      "end" : "2021-07-05"
+  }
+}
+
+/*Reactivate Occurrence (Disputed/Rejected)*/
+
+{
+  "enrolment_id" : " ",
+  "base_occurrence" : "1",
+  "reason" : ""
+}
+
+/*Reactivate Occurrence (Received)*/
+
+{
+  "enrolment_id" : " ",
+  "base_occurrence" : "1",
+  "child_crn" : "",
+  "child_dob" : "",
+  "child_first_name" : "",
+  "child_last_name" : "",
+  "individual_first_name" : "",
+  "individual_last_name" : "",
+  "individual_crn" : "",
+  "individual_dob" : "",
+  "reason" : ""
+}
